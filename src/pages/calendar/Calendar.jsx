0ea436b13bf5d5294302c0ca5f614be4402aa20f
Radwano01 <127989522+Radwano01@users.noreply.github.com>
2024-03-05 02:32:31 +0300

    Update Calendar.jsx

--- src/pages/calendar/Calendar.jsx
+++ src/pages/calendar/Calendar.jsx
@@ -23,17 +23,15 @@ const Calendar = () => {
             }
         };
         fetchData();
-    }, []);
+    }, [email]); // Include email as a dependency for useEffect
 
     const month = new Date().getMonth() + 1;
     const numberOfDays = new Date(2024, month, 0).getDate();
     const dates = Array.from({ length: numberOfDays }, (_, i) => i + 1);
     const getDaysStartingFromFirstDay = () => {
-        const firstDayOfMonth = new Date(2024, month, 0).getDay();
+        const firstDayOfMonth = new Date(2024, month - 1, 1).getDay(); // Adjust month to start from 0
         const daysStartingFromFirstDay = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
         const rotatedDays = [...daysStartingFromFirstDay.slice(firstDayOfMonth), ...daysStartingFromFirstDay.slice(0, firstDayOfMonth)];
-
         return rotatedDays;
     };
 
@@ -44,17 +42,13 @@ const Calendar = () => {
         const formattedEndDate = `${task?.endDate?.slice(6, 10)}-${task.endDate.slice(3, 5)}-${task.endDate.slice(0, 2)}`;
         const startDate = new Date(formattedStartDate);
         const endDate = new Date(formattedEndDate);
-
-        const currentDate = new Date(`2024-02-${date}`);
-        startDate.setHours(0, 0, 0, 0);
-        endDate.setHours(0, 0, 0, 0);
-        currentDate.setHours(0, 0, 0, 0);
+        const currentDate = new Date(2024, month - 1, date); // Adjust month to start from 0
         return currentDate.getTime() >= startDate?.getTime() && currentDate?.getTime() <= endDate?.getTime();
     };
 
     const handleNavigateToTaskDetails = (id)=>{
-        navigate(`/homepage/task-details/${id}/${email}`)
-    }
+        navigate(`/homepage/task-details/${id}/${email}`);
+    };
 
     return (
         <div className="calendar">
@@ -80,10 +74,8 @@ const Calendar = () => {
                                         )}
                                     </div>
                                 ))}
-
                             </td>
                         ))}
-
                     </tr>
                 </tbody>
             </table>
